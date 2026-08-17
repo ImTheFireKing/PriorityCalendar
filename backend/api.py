ed43a77ab6f8f287a1d52c0d06d8d76e55e713bf
ImTheFireKing <firekingisawesome@gmail.com>
2026-08-17 14:52:44 -0500

    Attempting to fix an issue preventing CORS from working on either site

--- backend/api.py
+++ backend/api.py
@@ -34,7 +34,7 @@ def _validateYear(year_str: str, allow_past: bool = False):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
-    allow_origin_regex=r"https://[a-zA-Z0-9-]*prioritycalendar[a-zA-Z0-9-]*\.vercel\.app",
+    allow_origin_regex=r"https://[a-zA-Z0-9-]*priority-?calendar[a-zA-Z0-9-]*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
