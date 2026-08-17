c965fbd3978685c4f6c5d51176c3633d39318d12
ImTheFireKing <firekingisawesome@gmail.com>
2026-08-17 14:35:36 -0500

    Added the ability for alternate links to work with CORS

--- backend/api.py
+++ backend/api.py
@@ -15,14 +15,10 @@ from fastapi import Response
 app = FastAPI()
 router = APIRouter(prefix="/api")
 
-origins = [
-    "http://localhost:5173",
-    "https://priority-calendar-trial-p3ns.vercel.app"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:5173"],
+    allow_origin_regex=r"https://[a-zA-Z0-9-]*prioritycalendar[a-zA-Z0-9-]*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
